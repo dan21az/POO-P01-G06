7cb18f54a2fd33f0ff15cfde19a10481cc151fa6
Marcelo092 <alan099727@gmail.com>
2025-11-10 21:32:25 -0500

    Update RegistroSostenible.java

--- app-estudiantil/src/main/java/com/mycompany/app/estudiantil/modelo/Sostenibilidad/RegistroSostenible.java
+++ app-estudiantil/src/main/java/com/mycompany/app/estudiantil/modelo/Sostenibilidad/RegistroSostenible.java
@@ -1,1 +1,31 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+public class RegistroSostenible {
+    private LocalDate fecha;
+    private ArrayList<String> acciones;
+    private int puntos;
+
+    public RegistroSostenible() {
+        this.fecha = LocalDate.now();
+        this.acciones = new ArrayList<>();
+        this.puntos = 0;
+    }
+
+    public void agregarAccion(String a) {
+        acciones.add(a);
+        puntos++;
+    }
+
+    public ArrayList<String> getAcciones() {
+        return acciones;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+}
