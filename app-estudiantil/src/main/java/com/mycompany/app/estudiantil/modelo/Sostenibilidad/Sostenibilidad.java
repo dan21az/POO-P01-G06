a35e0581d3f34ec143a21866c140a3bd31173793
Marcelo092 <alan099727@gmail.com>
2025-11-10 21:37:36 -0500

    Update Sostenibilidad.java

--- app-estudiantil/src/main/java/com/mycompany/app/estudiantil/modelo/Sostenibilidad/Sostenibilidad.java
+++ app-estudiantil/src/main/java/com/mycompany/app/estudiantil/modelo/Sostenibilidad/Sostenibilidad.java
@@ -1,1 +1,18 @@
+import java.util.ArrayList;
+
+public class Sostenibilidad {
+    private ArrayList<RegistroSostenible> registros;
+
+    public Sostenibilidad() {
+        registros = new ArrayList<>();
+    }
+
+    public void agregarRegistro(RegistroSostenible r) {
+        registros.add(r);
+    }
+
+    public ArrayList<RegistroSostenible> getRegistros() {
+        return registros;
+    }
+}
 
