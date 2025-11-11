90e351d6c2b2f284a468f6cc681b76b40ead691f
Marcelo092 <alan099727@gmail.com>
2025-11-10 21:39:27 -0500

    Update ControladorSostenibilidad.java

--- app-estudiantil/src/main/java/com/mycompany/app/estudiantil/controlador/ControladorSostenibilidad.java
+++ app-estudiantil/src/main/java/com/mycompany/app/estudiantil/controlador/ControladorSostenibilidad.java
@@ -1,5 +1,61 @@
 package com.mycompany.app.estudiantil.controlador;
+import
 
 public class ControladorSostenibilidad {
+  import java.util.ArrayList;
+
+
+public class ControladorSostenibilidad {
+    private Sostenibilidad modelo;
+    private VistaSostenibilidad vista;
+
+    public ControladorSostenibilidad(Sostenibilidad modelo, VistaSostenibilidad vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+
+    public void menuSostenibilidad() {
+        int opcion;
+        do {
+            opcion = vista.mostrarMenuSostenibilidad();
+            switch (opcion) {
+                case 1:
+                    registrarAccionesDelDia();
+                    break;
+                case 2:
+                    vista.mostrarInformeSemanal(modelo.getRegistros());
+                    break;
+                case 3:
+                    System.out.println("\nVolviendo al menú principal...");
+                    break;
+                default:
+                    System.out.println("\nOpción inválida, intente nuevamente.");
+            }
+            if (opcion != 3) vista.pausa();
+        } while (opcion != 3);
+    }
+
+    private void registrarAccionesDelDia() {
+        RegistroSostenible registro = new RegistroSostenible();
+        ArrayList<String> acciones = new ArrayList<>();
+
+        int opcion;
+        do {
+            opcion = vista.mostrarSubmenuAcciones();
+            switch (opcion) {
+                case 1 -> acciones.add("Usé transporte público, bicicleta o caminé.");
+                case 2 -> acciones.add("No realicé impresiones.");
+                case 3 -> acciones.add("No utilicé envases descartables (usé mi termo/taza).");
+                case 4 -> acciones.add("Separé y reciclé materiales (vidrio, plástico, papel).");
+                case 5 -> System.out.println("\nSaliendo del registro de acciones...");
+                default -> System.out.println("Opción inválida, intente nuevamente.");
+            }
+        } while (opcion != 5);
+
+        for (String a : acciones) registro.agregarAccion(a);
+        modelo.agregarRegistro(registro);
+        vista.mostrarConfirmacionRegistro(registro);
+    }
+}
 
 }
